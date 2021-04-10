98d5053d3ef444d156f687bacc510002f29e5b15
Rustam Musin <jvmusin@gmail.com>
2021-04-10 23:08:05 +0400

    Add docs to main method

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,25 @@
 import jvmusin.interpreter.token.ProgramTokenReader
 import jvmusin.interpreter.token.SymbolQueue
 
+/**
+ * Replaces `\r\n` newlines with `\n` ones and drops trailing newlines in the [input] string.
+ */
 private fun fixInput(input: String) = input.replace("\r\n", "\n").trimEnd('\n')
 
+/**
+ * Runs the program.
+ *
+ * Reads all the input and then parses and runs the program.
+ */
 fun main() {
     val input = System.`in`.readAllBytes().decodeToString()
     println(main(fixInput(input)))
 }
 
+/**
+ * Runs the program, assuming that [input] has `\n` newline symbols and no trailing end-line symbols.
+ * This can be achieved by calling [fixInput].
+ */
 fun main(input: String) = try {
     val program = ProgramTokenReader.read(SymbolQueue(input)).toElement()
     program.validate()
