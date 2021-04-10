4b8d6abc724e6e8db2625d64a38e880a9eca1c82
Rustam Musin <jvmusin@gmail.com>
2021-04-10 17:52:04 +0400

    Add gcd, lcm and abs examples to IntegrationTests

--- src/test/kotlin/jvmusin/interpreter/IntegrationTests.kt
+++ src/test/kotlin/jvmusin/interpreter/IntegrationTests.kt
@@ -5,67 +5,56 @@ import io.kotest.matchers.shouldBe
 import main
 
 class IntegrationTests : StringSpec({
+    val gcdFunction = "gcd(a,b)={[(b=0)]?(a):(gcd(b,(a%b)))}"
+    val lcmFunction = "lcm(a,b)={((a*b)/gcd(a,b))}"
+    val absFunction = "abs(x)={[(x<0)]?((0-x)):(x)}"
+    fun buildProgram(program: String, vararg functions: String) = (functions.toList() + program).joinToString("\n")
+    fun runProgram(program: String, vararg functions: String) = main(buildProgram(program, *functions))
+
     "Calculator (2+2)" {
-        main("(2+2)").toInt() shouldBe 4
+        runProgram("(2+2)").toInt() shouldBe 4
     }
     "Calculator (2+((3*4)/5))" {
-        main("(2+((3*4)/5))").toInt() shouldBe 4
+        runProgram("(2+((3*4)/5))").toInt() shouldBe 4
     }
     "If Expression" {
-        main("[((10+20)>(20+10))]?(1):(0)").toInt() shouldBe 0
+        runProgram("[((10+20)>(20+10))]?(1):(0)").toInt() shouldBe 0
     }
     "Functions" {
-        val input = """
-            g(x)={(f(x)+f((x/2)))}
-            f(x)={[(x>1)]?((f((x-1))+f((x-2)))):(x)}
-            g(10)
-        """.trimIndent()
-        main(input).toInt() shouldBe 60
+        val g = "g(x)={(f(x)+f((x/2)))}"
+        val f = "f(x)={[(x>1)]?((f((x-1))+f((x-2)))):(x)}"
+        runProgram("g(10)", g, f).toInt() shouldBe 60
     }
     "SYNTAX ERROR" {
-        main("1 + 2 + 3 + 4 + 5") shouldBe "SYNTAX ERROR"
+        runProgram("1 + 2 + 3 + 4 + 5") shouldBe "SYNTAX ERROR"
     }
     "PARAMETER NOT FOUND" {
-        val input = """
-            f(x)={y}
-            f(10)
-        """.trimIndent()
-        main(input) shouldBe "PARAMETER NOT FOUND y:1"
+        val f = "f(x)={y}"
+        runProgram("f(10)", f) shouldBe "PARAMETER NOT FOUND y:1"
     }
     "FUNCTION NOT FOUND" {
-        val input = """
-            g(x)={f(x)}
-            g(10)
-        """.trimIndent()
-        main(input) shouldBe "FUNCTION NOT FOUND f:1"
+        val g = "g(x)={f(x)}"
+        runProgram("g(10)", g) shouldBe "FUNCTION NOT FOUND f:1"
     }
     "ARGUMENT NUMBER MISMATCH" {
-        val input = """
-            g(x)={(x+1)}
-            g(10,20)
-        """.trimIndent()
-        main(input) shouldBe "ARGUMENT NUMBER MISMATCH g:2"
+        val g = "g(x)={(x+1)}"
+        runProgram("g(10,20)", g) shouldBe "ARGUMENT NUMBER MISMATCH g:2"
     }
     "RUNTIME ERROR" {
-        val input = """
-            g(a,b)={(a/b)}
-            g(10,0)
-        """.trimIndent()
-        main(input) shouldBe "RUNTIME ERROR (a/b):1"
+        val g = "g(a,b)={(a/b)}"
+        runProgram("g(10,0)", g) shouldBe "RUNTIME ERROR (a/b):1"
     }
     "ARGUMENT NAMES NOT DISTINCT" {
-        val input = """
-            f(a,b,a)={b}
-            f(1,2,1)
-        """.trimIndent()
-        main(input) shouldBe "ARGUMENT NAMES NOT DISTINCT f:1"
+        val f = "f(a,b,a)={b}"
+        runProgram("f(1,2,1)", f) shouldBe "ARGUMENT NAMES NOT DISTINCT f:1"
     }
     "FUNCTION NAMES NOT DISTINCT" {
-        val input = """
-            f(a)={a}
-            f(c)={c}
-            f(1)
-        """.trimIndent()
-        main(input) shouldBe "FUNCTION NAMES NOT DISTINCT f:2"
+        val f1 = "f(a)={a}"
+        val f2 = "f(c)={c}"
+        runProgram("f(1)", f1, f2) shouldBe "FUNCTION NAMES NOT DISTINCT f:2"
     }
+
+    "gcd function" { runProgram("gcd(3120,1352)", gcdFunction).toInt() shouldBe 104 }
+    "lcm function" { runProgram("lcm(3120,1352)", lcmFunction, gcdFunction).toInt() shouldBe 40560 }
+    "abs function" { runProgram("abs(-12)", absFunction).toInt() shouldBe 12 }
 })
